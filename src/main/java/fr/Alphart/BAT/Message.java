973910a5e8e4286c87fca20888d196a36920f3a7
Alphart <alphartdev@gmail.com>
2014-03-05 12:50:57 +0100

    Add message file cf javadoc description to see its usage

---
+++ src/main/java/fr/Alphart/BAT/Message.java
@@ -0,0 +1,11 @@
+package fr.Alphart.BAT;
+
+/**
+ * Group together all the messages used by several class at the time
+ */
+public class Message {
+	public static final String SPECIFY_SERVER = "&cYou must specify a server!";
+	public static final String INVALID_SERVER = "&cThe specified server is invalid!";
+	public static final String IP_OFFLINE_PLAYER = "&cThe player must be connected to perfom IP related operation.";
+	
+}
