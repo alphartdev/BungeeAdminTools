53d25dc542a4840db0476dfd5f4054d78cbc3019
helptiger <helptiger@users.noreply.github.com>
2015-05-28 09:19:42 +0200

    Update SQLQueries.java
    uniform table names (bat_comments --> BAT_comments)

--- src/main/java/fr/Alphart/BAT/database/SQLQueries.java
+++ src/main/java/fr/Alphart/BAT/database/SQLQueries.java
@@ -256,7 +256,7 @@ public class SQLQueries {
 	}
 
 	public static class Comments{
-		public static final String table = "bat_comments";
+		public static final String table = "BAT_comments";
 		
 		public static final String createTable = "CREATE TABLE IF NOT EXISTS `" + table + "` ("
 		+ "`id` int(11) NOT NULL AUTO_INCREMENT,"
