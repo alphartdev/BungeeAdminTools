7b39bca7e00a9bd7f81eae89171bad377c57ad65
Alphart <alphartdev@gmail.com>
2016-03-02 19:52:03 +0100

    Jump version test for all forks

--- src/main/java/fr/Alphart/BAT/BAT.java
+++ src/main/java/fr/Alphart/BAT/BAT.java
@@ -58,15 +58,15 @@ public class BAT extends Plugin {
 	public void onEnable() {
 		instance = this;
 		config = new Configuration();
-
-		if(!ProxyServer.getInstance().getName().equals("Waterfall")) {
-			if (getBCBuild() < requiredBCBuild) {
-				getLogger().severe("Your BungeeCord build (#" + getBCBuild() + ") is not supported. Please use at least BungeeCord #" + requiredBCBuild);
-				getLogger().severe("BAT is going to shutdown ...");
-				return;
-			}
-		} else {
-			getLogger().warning("BungeeCord version check disabled because you are using Waterfall");
+		getLogger().setLevel(Level.INFO);
+		if (!ProxyServer.getInstance().getName().equals("BungeeCord")) {
+		  getLogger().warning("BungeeCord version check disabled because a fork has been detected."
+              + " Make sur your fork is based on a BungeeCord build > #" + requiredBCBuild);
+		}
+		else if(getBCBuild() < requiredBCBuild){
+		  getLogger().severe("Your BungeeCord build (#" + getBCBuild() + ") is not supported. Please use at least BungeeCord #" + requiredBCBuild);
+          getLogger().severe("BAT is going to shutdown ...");
+          return;
 		}
 		if(config.isDebugMode()){
 		    try{
