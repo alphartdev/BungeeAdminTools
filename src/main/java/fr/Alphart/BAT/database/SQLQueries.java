7a46a3c652f1eb7ea5a50f0a1a78fec33310012a
Alphart <alphartdev@gmail.com>
2016-07-05 02:32:06 +0200

    Set BAT_player attribute to UNIQUE for supporting offline mode server

--- src/main/java/fr/Alphart/BAT/database/SQLQueries.java
+++ src/main/java/fr/Alphart/BAT/database/SQLQueries.java
@@ -309,7 +309,7 @@ public class SQLQueries {
 		public static final String table = "BAT_players";
 
 		public static final String createTable = "CREATE TABLE IF NOT EXISTS `" + table + "` ("
-				+ "`BAT_player` varchar(30) NOT NULL," + "`UUID` varchar(100) UNIQUE NOT NULL,"
+				+ "`BAT_player` varchar(30) UNIQUE NOT NULL," + "`UUID` varchar(100) UNIQUE NOT NULL,"
 				+ "`lastip` varchar(50) NOT NULL," + "`firstlogin` timestamp NULL,"
 				+ "`lastlogin` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP," + "INDEX(BAT_player)"
 				+ ") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
